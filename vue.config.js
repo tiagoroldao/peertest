1ac7f8f7dbf1099151b834699eededd28c8a6fe4
Tiago Roldao <tiago.roldao@infinite.software>
2019-06-04 21:24:49 +0200

    Fixed deploy settings

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  lintOnSave: false,
+    lintOnSave: false,
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/peertest/'
+        : '/',
+    configureWebpack: (config) => {
+        if (process.env.NODE_ENV === 'development') {
+            config.devtool = 'eval-source-map';
+            config.output.devtoolFallbackModuleFilenameTemplate = 'webpack:///[resource-path]?[hash]';
+            config.output.devtoolModuleFilenameTemplate = (info) => {
+                const isVue = info.resourcePath.match(/\.vue$/);
+                const isScript = info.identifier.match(/type=script/);
+                return isVue && !isScript
+                    ? `webpack-vue:///${info.resourcePath}`
+                    : `webpack-generated:///${info.resourcePath}?${info.hash}`;
+            };
+        }
+    },
 };
